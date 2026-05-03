8c47d6a0f58cee3f310d6e2d95923ea65b18ae4a
xnm <artem.shv@proton.me>
2026-05-03 17:38:45 +0300

    chore(nixos): 🔧 export Android SDK paths
    Add `ANDROID_SDK_ROOT` and `ANDROID_AVD_HOME` to the NixOS
    environment variables using the configured user home path.
    
    Why: makes Android SDK and emulator tooling discoverable from the
    system environment without hardcoding the home directory.

--- nixos/environment-variables.nix
+++ nixos/environment-variables.nix
@@ -1,8 +1,10 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   # Setup Env Variables
   environment.variables.SPOTIFY_PATH = "${pkgs.spotify}/";
   environment.variables.JDK_PATH = "${pkgs.jdk17}/";
   environment.variables.NODEJS_PATH = "${pkgs.nodejs}/";
+  environment.variables.ANDROID_SDK_ROOT = "${config.users.users.xnm.home}/android-sdk";
+  environment.variables.ANDROID_AVD_HOME = "${config.users.users.xnm.home}/.android/avd";
 }
